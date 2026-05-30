f2248c7492d9fe3e7691afc4d1ead572c73fe393
Lordradeez <lordradez362@gmail.com>
2026-05-30 17:08:11 +0100

    security: implement BCrypt password hashing on registration and login

--- src/main/java/com/lordradeez/services/UserService.java
+++ src/main/java/com/lordradeez/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.lordradeez.entities.User;
@@ -15,58 +16,61 @@ import com.lordradeez.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
-	@Autowired
-	UserRepository userRepo;
-	
-	@Autowired
-	UserOTPRepository userOtpRepo;
-	
-	@Autowired
-	JavaMailSender mailSender;
-	
-	public void register(User user) {
-		userRepo.save(user);
-	}
-	
-	public boolean loginAndGenerateOTP(String emailId, String password) {
-		User user = userRepo.findByEmailId(emailId);
-		if(user == null) {
-			return false;
-		}
-		
-		if(!user.getPassword().equals(password)) {
-			return false;
-		}
-		
-		int otpNum = new Random().nextInt(100000,1000000);
-		String otp = String.valueOf(otpNum);
-		
-		UserOtp userOtp = new UserOtp();
-		userOtp.setOtp(otp);
-		userOtp.setUserId(user.getId());
-		userOtp.setCreatedTime(LocalDateTime.now());
-		userOtpRepo.save(userOtp);
-		
-		
-		
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setTo(user.getEmailId());
-		message.setSubject("Your Otp Code");
-		message.setText("Hello " + user.getName() + ". Your Otp Code is : " + otp + "This otp is valid for 1 minute");
-		mailSender.send(message);
-		return true;
-	}
-	
-	public boolean verifyOtp(String otp) {
-		UserOtp userOtp = userOtpRepo.findByOtp(otp);
-		if(userOtp == null) {
-			return false;
-		} 
-		LocalDateTime expiryTime = userOtp.getCreatedTime().plusMinutes(1);
-		if(LocalDateTime.now().isAfter(expiryTime)) { 
-			return false;
-		}
-		return true;
-	}
+
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Autowired
+    UserRepository userRepo;
+
+    @Autowired
+    UserOTPRepository userOtpRepo;
+
+    @Autowired
+    JavaMailSender mailSender;
+
+    public void register(User user) {
+        // Hash the password before saving
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepo.save(user);
+    }
+
+    public boolean loginAndGenerateOTP(String emailId, String password) {
+        User user = userRepo.findByEmailId(emailId);
+        if (user == null) {
+            return false;
+        }
+
+        // Compare BCrypt hashed password
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            return false;
+        }
+
+        int otpNum = new Random().nextInt(100000, 1000000);
+        String otp = String.valueOf(otpNum);
+
+        UserOtp userOtp = new UserOtp();
+        userOtp.setOtp(otp);
+        userOtp.setUserId(user.getId());
+        userOtp.setCreatedTime(LocalDateTime.now());
+        userOtpRepo.save(userOtp);
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(user.getEmailId());
+        message.setSubject("Your LordAuth OTP Code");
+        message.setText("Hello " + user.getName() + ",\n\nYour OTP code is: " + otp + "\n\nThis code is valid for 1 minute.\n\n— LordAuth System");
+        mailSender.send(message);
+        return true;
+    }
+
+    public boolean verifyOtp(String otp) {
+        UserOtp userOtp = userOtpRepo.findByOtp(otp);
+        if (userOtp == null) {
+            return false;
+        }
+        LocalDateTime expiryTime = userOtp.getCreatedTime().plusMinutes(1);
+        if (LocalDateTime.now().isAfter(expiryTime)) {
+            return false;
+        }
+        return true;
+    }
 }
